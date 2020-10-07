94641101328cb46301f1a45ab7b1da2c81a3ca45
Amanda Nelson <amanda.gaylord@gmail.com>
2020-10-06 20:46:42 -0400

    Attempting to get the function to actually show on the website.

--- components/Article.js
+++ components/Article.js
@@ -10,14 +10,12 @@ const data = [
         watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
         Hutt calamari darth jabba.Darth dooku amidala organa moff.Boba darth binks solo hutt skywalker dantooine skywalker.Qui - gonn
         jar twi'lek jinn leia jango skywalker mon.`,
-
     secondParagraph: `Grievous fett calamari anakin skywalker hutt.Alderaan darth kenobi darth r2- d2
         windu mothma.Sidious darth calamari moff.Wampa mothma sith wedge solo mara.Darth gonk maul sith moff chewbacca palpatine
         mace amidala.C - 3po solo skywalker anakin yoda leia.Maul wampa bespin watto jade ewok darth jabba.Lando dantooine moff
         k - 3po dantooine luke.Fisto mandalore darth wedge c - 3p0 ahsoka.Secura moff palpatine fett.Anakin sith darth darth.Moff
         solo leia ben ponda jade.Binks jango aayla skywalker skywalker cade.Mustafar darth ventress anakin watto.Yavin jawa sebulba
         owen jinn tatooine sith organa.`,
-
     thirdParagraph: `Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
         naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff baba wicket
         han. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi'lek padmé wookiee. Leia
@@ -32,12 +30,10 @@ const data = [
         yew pumpkin juice phials Ravenclaw’s Diadem 10 galleons Thieves Downfall. Ministry-of-Magic mimubulus mimbletonia Pigwidgeon
         knut phoenix feather other minister Azkaban. Hedwig Daily Prophet treacle tart full-moon Ollivanders You-Know-Who cursed.
         Fawkes maze raw-steak Voldemort Goblin Wars snitch Forbidden forest grindylows wool socks`,
-
     secondParagraph: `Boggarts lavender robes, Hermione Granger Fantastic Beasts and Where to Find Them. Bee in your bonnet Hand of Glory elder
         wand, spectacles House Cup Bertie Bott’s Every Flavor Beans Impedimenta. Stunning spells tap-dancing spider Slytherin’s Heir
         mewing kittens Remus Lupin. Palominos scarlet train black robes, Metamorphimagus Niffler dead easy second bedroom. Padma
         and Parvati Sorting Hat Minister of Magic blue turban remember my last.`,
-
     thirdParagraph: `Toad-like smile Flourish and Blotts he knew I’d come back Quidditch World Cup. Fat Lady baubles banana fritters fairy lights 
         Petrificus Totalus. So thirsty, deluminator firs’ years follow me 12 inches of parchment. Head Boy start-of-term banquet Cleansweep Seven 
         roaring lion hat. Unicorn blood crossbow mars is bright tonight, feast Norwegian Ridgeback. Come seek us where our voices sound, we cannot 
@@ -55,7 +51,6 @@ const data = [
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Weedle Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Kakuna Lorem ipsum dolor sit amet, consectetur adipiscing elit. Beedrill Lorem ipsum dolor sit amet, consectetur adipiscing
         elit.`,
-
     secondParagraph: `Pidgey Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pidgeotto Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Pidgeot Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rattata Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Raticate Lorem ipsum dolor sit amet, consectetur adipiscing elit. Spearow Lorem ipsum dolor sit amet, consectetur adipiscing
@@ -66,7 +61,6 @@ const data = [
         adipiscing elit. Nidorina Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidoqueen Lorem ipsum dolor sit amet,
         consectetur adipiscing elit. Nidoran Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidorino Lorem ipsum dolor
         sit amet, consectetur adipiscing elit. Nidoking Lorem ipsum`,
-
     thirdParagraph: `Gotta catch 'em all Horsea gym Ninjask Absol Sinnoh Poliwag. Gotta catch 'em all Youngster wants to fight Soda Pop Floatzel 
         Leech Life Seismitoad Ariados. Earthquake Pokemon Glitch City Tail Whip Skitty Ekans Dialga. Ut aliquip ex ea commodo consequat James 
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`,
@@ -77,7 +71,6 @@ const data = [
     firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
           hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
           Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
-
     secondParagraph: `Hodor, hodor. Hodor. Hodor, hodor, hodor. Hodor hodor, hodor. Hodor hodor, hodor, hodor hodor. Hodor! Hodor hodor, hodor;
           hodor hodor hodor? Hodor, hodor. Hodor. Hodor, hodor - HODOR hodor, hodor hodor hodor! Hodor, hodor. Hodor. Hodor, HODOR
           hodor, hodor hodor, hodor, hodor hodor. Hodor hodor - hodor - hodor... Hodor hodor hodor hodor hodor hodor hodor?! Hodor
@@ -88,64 +81,61 @@ const data = [
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`,
   },
 ];
-
-/*
-  Step 1: Write a component called 'articleMaker' to create an article.
+/*  Step 1: Write a component called 'articleMaker' to create an article.
   Your component is a function that takes an article object as its only argument,
   and returns a DOM node looking like the one below: 
-
   <div class="article">
     <h2>{title of the article}</h2>
     <p class="date">{date of the article}</p>
-
     {three separate paragraph elements}
-
     <span class="expandButton">+</span>
   </div> */
 
-const articleMaker = (article, divParent) => {
-  const div = document.createElement("div .article");
+const articleMaker = (divParent) => {
+  const divParent = document.createElement("div");
+  divParent.classList.add("article");
   const title = document.createElement("h2");
-  const date = document.createElement("p .date");
-  const p1 = document.createElement("p .firstParagraph");
-  const expand = document.createElement("span .expandButton");
-  const p2 = document.createElement("p .secondParagraph");
-  const p3 = document.createElement("p .thirdParagraph");
+  const date = document.createElement("p");
+  date.classList.add("date");
+  const p1 = document.createElement("p");
+  p1.classList.add("firstParagraph");
+  const p2 = document.createElement("p");
+  p2.classList.add("secondParagraph");
+  const p3 = document.createElement("p");
+  p3.classList.add("thirdParagraph");
+  const expand = document.createElement("span");
+  expand.classList.add("expandButton");
   title.textContent = data.title;
   date.textContent = data.date;
   p1.textContent = data.firstParagraph;
   p2.textContent = data.secondParagraph;
   p3.textContent = data.thirdParagraph;
-  div.appendChild(title);
-  div.appendChild(date);
-  div.appendChild(p1);
-  div.appendChild(expand);
-  div.appendChild(p2);
-  div.appendChild(p3);
+  divParent.appendChild(title);
+  //expand button needs to be under the title to be visible.
+  divParent.appendChild(expand);
+  divParent.appendChild(date);
+  divParent.appendChild(p1);
+  divParent.appendChild(p2);
+  divParent.appendChild(p3);
   //event listener for the extend "+"
-  const expandedPs = expand.addEventListener("click", (e) => {
-    div.classList.toggle("article-open");
+  expand.addEventListener("click", (e) => {
+    //toggle so the
+    divParent.classList.toggle("article-open");
   });
-
-  return div;
+  return divParent;
 };
-
 //telling the system what an article is.
-const articleData = document.querySelector("div .article");
-
+const articles = document.querySelector(".articles");
 //explaining that for each of the objects in the data array, the system should activate the articlaMaker above.
 data.forEach((article) => {
-  articleMaker(article, articleData);
+  const a = articleMaker(articles, divParent);
+  articles.appendChild(a);
 });
-
-/* Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
+/* Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton. ✅
   This listener should toggle the class 'article-open' on div.article.
-
-  Step 3: Don't forget to return something from your function!
-
+  Step 3: Don't forget to return something from your function! ✅
   Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
-  to create a div.article element and append it to the DOM inside div.articles (see index.html).
-
+  to create a div.article element and append it to the DOM inside div.articles (see index.html). ✅
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
