f87655b833a7a0e9241ffc2de9ae69e257eb27a0
Amanda Nelson <amanda.gaylord@gmail.com>
2020-10-07 00:53:57 -0400

    stretch animations added to menu.js

--- components/Menu.js
+++ components/Menu.js
@@ -19,7 +19,7 @@ let menuItems = [
 const menuMaker = (array) => {
   const button = document.querySelector(".menu-button");
   const menu = document.createElement("div");
-  menu.classList("menu");
+  menu.classList.add("menu");
   const ul = document.createElement("ul");
   array.forEach((element) => {
     const index = document.createElement("li");
@@ -34,10 +34,17 @@ const menuMaker = (array) => {
 };
 const header = document.querySelector(".header");
 header.appendChild(menuMaker(menuItems));
+// Stretch Goal: Animation for menu image:
+document.querySelector(".menu-button").addEventListener("mouseover", (e) => {
+  e.target.style =
+    "transform: skew(20deg, 10deg) scale(1.2);  transition: 0.5s ease-in;";
+});
+document.querySelector(".menu-button").addEventListener("mouseout", (e) => {
+  e.target.style = "";
+});
+// The 'menuMaker' takes an array of menu items as its only argument.
 
-/* The 'menuMaker' takes an array of menu items as its only argument.
-
-  Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array. ✅
+/* Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array. ✅
   Add those items to the <ul> ✅
 
   Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button'). ✅
